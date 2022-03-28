69a441114656aaac22456956c15a507a990e5169
Bien Pham <biendz2000@gmail.com>
2022-03-28 08:21:38 +0700

    assce

---
+++ src/com/company/Circle.java
@@ -0,0 +1,38 @@
+package com.company;
+
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    private final double PI = 3.14159;
+
+    public Circle(double radius, String red, double v) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double area(){
+        return PI*this.radius*this.radius;
+    }
+    public double perimeter(){
+        return 2*PI*this.radius;
+    }
+}
